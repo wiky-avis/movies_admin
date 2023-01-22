1fba839362e22c6019f78aededeec49433d52eab
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-01-22 21:29:28 +0300

    добавлена зависимость django-split-settings, перенесены настройки бд в одтельный файл database.py

--- movies_admin/config/settings.py
+++ movies_admin/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from split_settings.tools import include
 
 load_dotenv()
 
@@ -72,25 +73,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', 5432),
-        'OPTIONS': {
-            # Нужно явно указать схемы, с которыми будет работать приложение.
-           'options': '-c search_path=public,content'
-        }
-    }
-}
-
+include(
+    'components/database.py',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
