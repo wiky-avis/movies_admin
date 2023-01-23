5174a69baf3695dcf461a708e815323a53646570
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-01-23 20:04:11 +0300

    добавлены модели Genre и Filmwork

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Genre, Filmwork
 
-# Register your models here.
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    pass
--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -1,3 +1,36 @@
+import uuid
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Genre(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField('name', max_length=255)
+    description = models.TextField('description', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "content\".\"genre"
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Filmwork(models.Model):
+    class Types(models.TextChoices):
+        MOVIE = 'movie', _('Фильм')
+        TV_SHOW = 'tv_show', _('Телешоу')
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField('title', max_length=255)
+    description = models.TextField('description', blank=True)
+    creation_date = models.DateField('creation_date')
+    rating = models.FloatField('rating')
+    type = models.CharField(max_length=7, choices=Types.choices, default=Types.MOVIE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "content\".\"film_work"
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
