5210cca538c24e123933a8aa4ae3efee0f573417
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-01-24 09:17:35 +0300

    добавила настройки для моделей в админке

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -5,24 +5,28 @@ from .models import Genre, Filmwork, GenreFilmwork, Person, PersonFilmwork
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', )
+    search_fields = ('name', 'id')
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('id', 'full_name', )
+    search_fields = ('full_name', 'id')
 
 
 class GenreFilmworkInline(admin.TabularInline):
     model = GenreFilmwork
+    extra = 0
 
 
 class PersonFilmworkInline(admin.TabularInline):
     model = PersonFilmwork
+    extra = 0
 
 
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
     inlines = (GenreFilmworkInline, PersonFilmworkInline,)
-    list_display = ('id', 'title', 'type', 'creation_date', 'rating',)
+    list_display = ('title', 'type', 'creation_date', 'rating',)
     list_filter = ('type', 'creation_date', 'rating', 'genres', 'persons',)
     search_fields = ('title', 'description', 'id',)
