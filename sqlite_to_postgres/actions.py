70103495ceebc6b50e597a0c9f765387b284078f
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-01-29 16:57:13 +0300

    добавила тайпинги

--- sqlite_to_postgres/actions.py
+++ sqlite_to_postgres/actions.py
@@ -1,7 +1,9 @@
 import logging
 import sqlite3
-from dataclasses import asdict
+from dataclasses import asdict, dataclass
+from typing import Any, Iterator, List, Tuple
 
+import psycopg2
 from consts import CHANK
 from models import TableName
 from psycopg2.extras import execute_batch
@@ -12,7 +14,9 @@ class SQLiteExtractor:
         self.conn = conn
         self.cursor = self.conn.cursor()
 
-    def get_queries_chunk(self, table_name, model):
+    def get_queries_chunk(
+        self, table_name: str, model: dataclass
+    ) -> Iterator[List[dataclass]]:
         self.cursor.execute(f"SELECT * FROM {table_name} ORDER BY id")
         while True:
             records = self.cursor.fetchmany(CHANK)
@@ -20,7 +24,9 @@ class SQLiteExtractor:
                 break
             yield [model(*record) for record in records]
 
-    def extract_movies(self, table_name, model):
+    def extract_movies(
+        self, table_name: str, model: dataclass
+    ) -> Iterator[List[dataclass]]:
         try:
             yield self.get_queries_chunk(table_name, model)
         except sqlite3.Error:
@@ -36,7 +42,7 @@ class PostgresSaver:
         self.pg_conn = conn
         self.cursor = self.pg_conn.cursor()
 
-    def get_fields(self, table_name):
+    def get_fields(self, table_name: str) -> List[str]:
         return {
             TableName.FILM_WORK.value: [
                 "id",
@@ -72,10 +78,12 @@ class PostgresSaver:
             ],
         }.get(table_name)
 
-    def get_values(self, row, table_name):
+    def get_values(self, row: dict, table_name: str) -> Tuple[Any]:
         return tuple(row[field] for field in self.get_fields(table_name))
 
-    def save_all_data(self, pages, table_name):
+    def save_all_data(
+        self, pages: Iterator[List[dataclass]], table_name: str
+    ) -> None:
         for page in pages:
             fields = ", ".join(self.get_fields(table_name))
             values = ", ".join(["%s" for _ in self.get_fields(table_name)])
@@ -84,10 +92,17 @@ class PostgresSaver:
                 for records in page
                 for row in records
             ]
-            execute_batch(
-                self.cursor,
-                f"INSERT INTO {table_name}({fields}) VALUES({values})",
-                records,
-                page_size=CHANK,
-            )
+            try:
+                execute_batch(
+                    self.cursor,
+                    f"INSERT INTO {table_name}({fields}) VALUES({values})",
+                    records,
+                    page_size=CHANK,
+                )
+            except psycopg2.Error:
+                logging.error(
+                    "Failed to save data from postgres table %s",
+                    table_name,
+                    exc_info=True,
+                )
         self.pg_conn.commit()
