0ee94227936b93dc45129ec60300d1c8606059fe
vgaksentii <vgaksentii@domclick.ru>
2023-07-30 16:25:48 +0300

    переписываю скрипт на ассинхронные библы

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,23 +1,31 @@
+import asyncio
 import sqlite3
 
-import psycopg2
+import aiosqlite
+import psycopg
 from actions import PostgresSaver, SQLiteExtractor
 from consts import DSL, MODELS, TABLES
-from psycopg2.extensions import connection as _connection
-from psycopg2.extras import DictCursor
+from psycopg import AsyncConnection
 
 
-def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
+async def load_from_sqlite(
+    connection: sqlite3.Connection, pg_conn: AsyncConnection
+):
     """Основной метод загрузки данных из SQLite в Postgres"""
     for table_name, model in zip(TABLES, MODELS):
         sqlite_extractor = SQLiteExtractor(connection)
-        data = sqlite_extractor.extract_movies(table_name, model)
+        data = await sqlite_extractor.extract_movies(table_name, model)
         postgres_saver = PostgresSaver(pg_conn)
-        postgres_saver.save_all_data(data, table_name)
+        await postgres_saver.save_all_data(data, table_name)
+
+
+async def run():
+    async with aiosqlite.connect(
+        "db.sqlite"
+    ) as sqlite_conn, psycopg.AsyncConnection.connect(**DSL) as pg_conn:
+        sqlite_conn.row_factory = aiosqlite.Row
+        await load_from_sqlite(sqlite_conn, pg_conn)
 
 
 if __name__ == "__main__":
-    with sqlite3.connect("db.sqlite") as sqlite_conn, psycopg2.connect(
-        **DSL, cursor_factory=DictCursor
-    ) as pg_conn:
-        load_from_sqlite(sqlite_conn, pg_conn)
+    asyncio.run(run())
